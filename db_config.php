55115204a7371938a43fddd714b667cea1906f5b
John Mwaniki Wachira <75970978+Johnmwasmwaniki@users.noreply.github.com>
2024-11-07 14:46:52 +0300

    Delete db_config.php

--- db_config.php
+++
@@ -1,6 +0,0 @@
-<?php
-define('DB_HOST', 'localhost');  // Database host
-define('DB_USER', 'root');       // Database username
-define('DB_PASS', '');           // Database password
-define('DB_NAME', 'user_management'); // Database name
-?>
